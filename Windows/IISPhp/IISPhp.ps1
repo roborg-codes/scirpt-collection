0496195562ec1b664150445345ef07e119ba0a35
roborg-codes <80540375+roborg-codes@users.noreply.github.com>
2023-01-28 16:48:02 +0200

    Substitute for gist

--- Windows/IISPhp/IISPhp.ps1
+++ Windows/IISPhp/IISPhp.ps1
@@ -1,21 +1,21 @@
-Configuration IISPhp
+# https://gist.github.com/BernieWhite/23522e76765d8fa88db7abb5f03086ce#file-azure-web-vm-configuration-ps1
+
+configuration webConfiguration
 {
-    param
-    (
-        [string]$NodeName="localhost",
-        [string]$PhpProjectUrl
+    param (
+        [Parameter(Mandatory = $False)]
+        [String]$websitePackageUri
     )
 
-    Import-DscResource -ModuleName PsDesiredStateConfiguration
-    # Import-DscResource -ModuleName xWebAdministration
-    # Import-DscResource -ModuleName xPhp
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    # Import-DscResource -ModuleName WebAdministration
 
-    Node $NodeName
+    Node 'localhost'
     {
-        WindowsFeature WebServerRole
-        {
-            Ensure = "Present"
+        # Install IIS features
+        WindowsFeature WebServerRole {
             Name   = "Web-Server"
+            Ensure = "Present"
         }
 
         WindowsFeature WebManagementService {
@@ -23,12 +23,81 @@ Configuration IISPhp
             Ensure = "Present"
         }
 
-        File WebSite
-        {
-            Ensure            = "Present"
-            DestinationPath   = "C:\inetpub\wwwroot"
-            SourcePath        = $PhpProjectUrl
-            DependsOn         = "[WindowsFeature]WebServerRole"
+        WindowsFeature ASPNet45 {
+            Name   = "Web-Asp-Net45"
+            Ensure = "Present"
+        }
+
+        WindowsFeature HTTPRedirection {
+            Name   = "Web-Http-Redirect"
+            Ensure = "Present"
+        }
+
+        WindowsFeature CustomLogging {
+            Name   = "Web-Custom-Logging"
+            Ensure = "Present"
+        }
+
+        WindowsFeature LogginTools {
+            Name   = "Web-Log-Libraries"
+            Ensure = "Present"
+        }
+
+        WindowsFeature RequestMonitor {
+            Name   = "Web-Request-Monitor"
+            Ensure = "Present"
+        }
+
+        WindowsFeature Tracing {
+            Name   = "Web-Http-Tracing"
+            Ensure = "Present"
+        }
+
+        WindowsFeature BasicAuthentication {
+            Name   = "Web-Basic-Auth"
+            Ensure = "Present"
+        }
+
+        WindowsFeature WindowsAuthentication {
+            Name   = "Web-Windows-Auth"
+            Ensure = "Present"
+        }
+
+        WindowsFeature ApplicationInitialization {
+            Name   = "Web-AppInit"
+            Ensure = "Present"
+        }
+
+        if (![String]::IsNullOrEmpty($websitePackageUri)) {
+
+            # Download and unpack the website into the default website
+            Script DeployWebPackage {
+                GetScript  = {@{Result = "DeployWebPackage"}}
+                TestScript = {
+                    return Test-Path -Path "C:\WebApp\Site.zip";
+                }
+                SetScript  = {
+
+                    if (!(Test-Path -Path "C:\WebApp")) {
+                        New-Item -Path "C:\WebApp" -ItemType Directory -Force | Out-Null;
+                    }
+
+                    $dest = "C:\WebApp\Site.zip"
+
+                    if (Test-Path -Path "C:\inetpub\wwwroot") {
+                        Remove-Item -Path "C:\inetpub\wwwroot" -Force -Recurse -ErrorAction SilentlyContinue | Out-Null;
+                    }
+
+                    if (!(Test-Path -Path "C:\inetpub\wwwroot")) {
+                        New-Item -Path "C:\inetpub\wwwroot" -ItemType Directory -Force | Out-Null;
+                    }
+
+                    Invoke-WebRequest -Uri $using:websitePackageUri -OutFile $dest -UseBasicParsing;
+
+                    Expand-Archive -Path $dest -DestinationPath "C:\inetpub\wwwroot" -Force;
+                }
+                DependsOn  = "[WindowsFeature]WebServerRole"
+            }
         }
     }
 }
