a91d3ee460fff213c218c0995add307535aacf48
roborg-codes <80540375+roborg-codes@users.noreply.github.com>
2023-02-02 20:56:13 +0200

    Add port checking

--- Windows/MountFileShare/MountFileShare.ps1
+++ Windows/MountFileShare/MountFileShare.ps1
@@ -18,9 +18,6 @@ configuration FileShareConfiguration
     Import-DscResource -ModuleName PSDesiredStateConfiguration
 
     $FileShareUNCPath = "\\$using:StorageAccountName.file.core.windows.net\$using:FileShareName"
-    $Cred = New-Object System.Management.Automation.PSCredential(
-            $using:StorageAccountName,
-            $using:StorageAccountKey)
 
     Node 'localhost'
     {
@@ -35,13 +32,24 @@ configuration FileShareConfiguration
                 return [Bool](Get-PSDrive "X" -PSProvider "FileSystem")
             }
             SetScript = {
+                $ConnectTestResult = Test-NetConnection `
+                    -ComputerName $using:StorageAccountName.file.core.windows.net `
+                    -Port 445
+
+                if (-not $ConnectTestResult.TcpTestSucceeded) {
+                    Write-Error -Message "Unable to reach the Azure storage account via port 445."
+                    return
+                }
+
+                # Save the password so the drive will persist on reboot
+                cmd.exe /C "cmdkey /add:`"$using:FileShareUNCPath`" /user:`"localhost\$using:$StorageAccountName`" /pass:`"$using:StorageAccountKey`""
+
+                # Mount the drive
                 New-PSDrive `
                     -Persist `
-                    -Scope Global `
                     -Name "X" `
                     -PSProvider "FileSystem" `
-                    -Root $using:FileShareUNCPath `
-                    -Credential $using:Cred
+                    -Root $using:FileShareUNCPath
             }
         }
     }
