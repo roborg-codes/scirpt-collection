f83f5ecf22426c97620d556376cedb64ffd2be9a
roborg-codes <80540375+roborg-codes@users.noreply.github.com>
2023-03-01 18:57:37 +0200

    Reorder operations

--- Windows/SetupSsh.ps1
+++ Windows/SetupSsh.ps1
@@ -7,11 +7,11 @@ param(
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0;
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0;
 
-# Start the sshd service
-Start-Service sshd;
-
 # OPTIONAL but recommended: Set StartupType to automatic
 Set-Service -Name sshd -StartupType Automatic;
+
+# Now start the sshd service to generate default configuration
+Start-Service sshd;
 
 # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
 if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
@@ -21,15 +21,19 @@ if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyCon
     Write-Output "Firewall rule OpenSSH-Server-In-TCP has been created and exists.";
 }
 
+# Disable password authentication
+(Get-Content -Path $env:programdata\ssh\sshd_config).Replace(
+    "#PasswordAuthentication yes",
+    "PasswordAuthentication no").Replace(
+    "#PubkeyAuthentication yes",
+    "PubkeyAuthentication yes") | Set-Content -Path $env:programdata\ssh\sshd_config
+
 # Set default shell to PowerShell
 New-ItemProperty -Path HKLM:\SOFTWARE\OpenSSH -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force;
 
-# Set the sshd service to be started automatically
-Get-Service -Name sshd | Set-Service -StartupType Automatic;
+# Add our public key
+Write-Output $publicKey | Add-Content $env:programdata\ssh\administrators_authorized_keys;
+icacls.exe $env:programdata\ssh\administrators_authorized_keys /inheritance:r /grant Administrators:F /grant SYSTEM:F;
 
 # Now start the sshd service
-Start-Service sshd;
-
-# Add our public key
-Write-Output $publicKey | Add-Content C:\ProgramData\ssh\administrators_authorized_keys;
-icacls.exe C:\ProgramData\ssh\administrators_authorized_keys /inheritance:r /grant Administrators:F /grant SYSTEM:F;
+Restart-Service sshd;
